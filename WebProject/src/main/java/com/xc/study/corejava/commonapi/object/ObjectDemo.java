d1d162c0989f09e8b0c486d9612eef673a4b1ea4
xc <1948650335@qq.com>
2018-08-07 16:40:56 +0800

    ADD Object上帝类模块只是

---
+++ WebProject/src/main/java/com/xc/study/corejava/commonapi/object/ObjectDemo.java
@@ -0,0 +1,9 @@
+package com.xc.study.corejava.commonapi.object;
+
+public class ObjectDemo {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Object o = new Object();
+	}
+}
